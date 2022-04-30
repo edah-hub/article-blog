d651cad35339e009029f7eab2e9f14597dfd3bd1
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-30 14:52:57 +0300

    adds changes

--- flask-blog.py
+++ flask-blog.py
@@ -1,6 +1,9 @@
 from importlib.resources import contents
+from forms import RegistrationForm,LoginForm
 from flask import Flask,render_template,url_for
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] ='4db77598290f06523456cafce29ac3d9'
 
 
 posts = [
@@ -30,6 +33,17 @@ def about():
 @app.route("/contact_us/")
 def contact_us():
      return "<h1> Contact us page </h1>"
+ 
+@app.route("/register/")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html",title = "Registration", form=form)
+
+@app.route("/login/")
+def login():
+    form = RegistrationForm()
+    return render_template("register.html",title = "login", form=form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
